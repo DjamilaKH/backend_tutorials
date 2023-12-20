2d17392208d12d01de63d70e8499134b95b271e4
Jamila Nouri <jamila.nouri@esprit.tn>
2023-12-20 20:26:19 +0100

    final_JWT_back_nodejs

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "jamila-secret-key"
+  };
--- server.js
+++ server.js
@@ -17,18 +17,38 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
-db.sequelize.sync()
+const Role = db.role;
+db.sequelize.sync({force: true})
   .then(() => {
-    console.log("Synced db.");
+    console.log("'Drop and Resync Db'");
+    initial();
   })
   .catch((err) => {
     console.log("Failed to sync db: " + err.message);
   });
+  function initial() {
+    Role.create({
+      id: 1,
+      name: "user"
+    });
+   
+    Role.create({
+      id: 2,
+      name: "moderator"
+    });
+   
+    Role.create({
+      id: 3,
+      name: "admin"
+    });
+  }
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 require("./app/routes/tutorial.routes")(app);
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
